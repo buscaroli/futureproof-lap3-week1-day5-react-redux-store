c8d8faaf63d03f63fc563495f9b316d8de0bd47c
mabus <m.buscaroli@outlook.com>
2022-06-20 10:28:21 +0100

    install axios, create a api folder with a fakeStore file to fetch fake data, work on ShoppingNavbar

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { ShoppingNavbar } from '../../components'
+import styles from './index.module.css'
 
 function MainPage() {
   return (
-    <div>MainPage</div>
+    <div className={styles.wrapper}>
+      <ShoppingNavbar />
+      <div>ShoppingGrid</div>
+    </div>
   )
 }
 
