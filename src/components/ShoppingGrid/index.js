7a63cf144c98961f2b5a5ae8fcb2c3185f46527a
mabus <m.buscaroli@outlook.com>
2022-06-20 17:29:56 +0100

    can now show the items in the ShoppingGrid component, but they need to be styled

--- src/components/ShoppingGrid/index.js
+++ src/components/ShoppingGrid/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import { getItems } from '../../api/fakeStore'
+import { ItemCard } from '../index'
+
+import styles from './index.module.css'
 
 function ShoppingGrid() {
   const [storeItems, setStoreItems] = useState([])
@@ -16,10 +19,23 @@ function ShoppingGrid() {
 
   // console.log('*** list: ', storeItems)
   return (
-    <ul>
+    <ul className={styles.wrapper}>
+      {/* {storeItems === []
+        ? 'Getting The Items from the Online Store...'
+        : storeItems.map((item) => <li key={item.id}>{item.title}</li>)} */}
       {storeItems === []
         ? 'Getting The Items from the Online Store...'
-        : storeItems.map((item) => <li key={item.id}>{item.title}</li>)}
+        : storeItems.map((item) => (
+            <li key={item.id}>
+              <ItemCard
+                id={item.id}
+                title={item.title}
+                description={item.description}
+                price={item.price}
+                image={item.image}
+              />
+            </li>
+          ))}
     </ul>
   )
 }
