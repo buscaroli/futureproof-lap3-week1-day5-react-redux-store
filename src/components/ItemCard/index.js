c8f1794136b8ee9fdbe5e0880db0369aa2d52e94
mabus <m.buscaroli@outlook.com>
2022-06-20 21:12:37 +0100

    improve styling of ItemCard, add button to add item to shoppingCart, functionality to be implemented

--- src/components/ItemCard/index.js
+++ src/components/ItemCard/index.js
@@ -2,6 +2,13 @@ import React from 'react'
 import styles from './index.module.css'
 
 function ItemCard({ id, title, description, price, image }) {
+  const handleAddItem = (e) => {
+    e.preventDefault()
+
+    console.log('clicked ', e.target)
+    console.log('id is ', id)
+  }
+
   return (
     <div className={styles.wrapper}>
       <div className="pic">
@@ -11,6 +18,10 @@ function ItemCard({ id, title, description, price, image }) {
       <div className={styles.lowerBar}>
         <div className={styles.title}>{title}</div>
         <div className={styles.price}>{`£${price}`}</div>
+      </div>
+
+      <div onClick={handleAddItem} className={styles.floatingBtn}>
+        +
       </div>
     </div>
   )
