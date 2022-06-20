c6cd747ef96014b988467472dc0676f33b5e9026
mabus <m.buscaroli@outlook.com>
2022-06-20 14:14:03 +0100

    error: when adding the store through the provider the page has no content at all, the body tag is empty, it also happens by just importing the store

--- src/components/ShoppingGrid/index.js
+++ src/components/ShoppingGrid/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function ShoppingGrid() {
-  return <div>ShoppingGrid</div>
+  const balance = useSelector((state) => state.balance)
+
+  return <div>Balance: {balance}</div>
 }
 
 export default ShoppingGrid
