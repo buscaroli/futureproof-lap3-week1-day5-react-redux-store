a50bda6dc86f3cbdc01d2b43cf8b2a6ee8a02298
mabus <m.buscaroli@outlook.com>
2022-06-21 16:12:34 +0100

    fix a bug that prevents the main page to display the navbar at the top, add a test suite for the checkbutton - failing atm

--- src/components/ItemCard/index.js
+++ src/components/ItemCard/index.js
@@ -24,13 +24,15 @@ function ItemCard({ id, title, description, price, image }) {
   }
 
   return (
-    <div className={styles.wrapper}>
+    <div role="comment" className={styles.wrapper}>
       <div className="pic">
         <img className={styles.img} src={image} alt={description} />
       </div>
 
       <div className={styles.lowerBar}>
-        <div className={styles.title}>{title}</div>
+        <div data-testid="cardTitle" className={styles.title}>
+          {title}
+        </div>
         <div className={styles.price}>{`£${price}`}</div>
       </div>
 
