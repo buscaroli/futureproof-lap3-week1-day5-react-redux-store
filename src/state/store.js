3fd8f2a6836234bb0d5c418dae02ec1a76b66393
mabus <m.buscaroli@outlook.com>
2022-06-21 16:21:33 +0100

    add compose to the store in order to be able to see the state in redux-dev-tools

--- src/state/store.js
+++ src/state/store.js
@@ -1,10 +1,11 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { shoppingReducer } from './reducers/shoppingReducer'
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 export const myStore = createStore(
   combineReducers({
     shoppingReducer,
   }),
-  applyMiddleware(thunk)
+  composeEnhancers(applyMiddleware(thunk))
 )
